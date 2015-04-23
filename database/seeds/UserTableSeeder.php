7579726efdbba0f04db086c3f9774c3856479284
Gede Wahyu Adi Pramana <gedewahyu.ap@gmail.com>
2015-04-23 11:38:28 +0700

    move user model to models dir

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
   use Illuminate\Database\Seeder;
   use Illuminate\Database\Eloquent\Model;
-  use App\User;
+  use App\Models\User;
   class UserTableSeeder extends Seeder {
     public function run(){
       DB::table('users')->delete();
