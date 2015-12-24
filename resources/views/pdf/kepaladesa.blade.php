20516441c984ab8b8fa8733783bcd1f2fae2f0cc
Teresa Pranyoto <teresapranyoto@gmail.com>
2015-12-24 18:12:50 +0700

    surat 1-5: surat kepala desa, permohonan, surat kuasa

---
+++ resources/views/pdf/kepaladesa.blade.php
@@ -0,0 +1,178 @@
+<html>
+    <head>
+        <title></title>
+        <style type="text/css">
+            table {
+                table-layout: auto;
+                border-collapse: collapse;
+            }
+            table td.absorbing-column {
+                width: 100%;
+            }
+            body {
+            font-family: 'times new roman';
+            }
+        </style>
+    </head>
+    <body>
+        <h2 style="text-align: center;">
+            <strong>PEMERINTAH KABUPATEN BANDUNG BARAT</strong></h2>
+        <table style="margin-left:auto;margin-right:auto;">
+            <tbody>
+                <tr>
+                    <td>
+                        KECAMATAN</td>
+                    <td>
+                        : kec</td>
+                </tr>
+                <tr>
+                    <td>
+                        DESA / KELURAHAN</td>
+                    <td style="text-align: center;">
+                        : desa</td>
+                </tr>
+            </tbody>
+        </table>
+        <h3 style="text-align: center;">
+            <u><strong>SURAT KETERANGAN KEPALA DESA*</strong></u></h3>
+        <center>
+            Nomor : #no#</center>
+        <br />
+        <p>
+            Yang bertanda tangan di bawah ini :</p>
+        <p>
+            Kepala Desa / Lurah {{###}} Kecamatan {{###}} Kabupaten Bandung Barat, dengan ini menerangkan, bahwa Blok/Jalan/Kampung {{###}} PersilNo {{###}} KohirNo.{{###}} Luas &plusmn; {{###}} m<sup>2</sup> tertulis atas nama {{###}} Terletak di Desa/Kel {{###}} Kecamatan {{###}} Kabupaten Bandung Barat, sampai saat ini :</p>
+        <ol>
+            <li>
+                Adalah benar <strong><em>dikuasai</em></strong> oleh {{###}} sejak {{###}} dengan dasar penguasaan berupa&nbsp; {{###}} yang sebelumdimilikinya, telah dikuasai oleh pemilik-pemilik pendahulunya selama 20 tahun berturut-turut secara nyata/tidak dijaminkan / tidak sengketa.</li>
+            <li>
+                Benar sampai saat ini tidak ada permasalahan/sengketa baik batas-batas maupun kepemilikannya,</li>
+            <li>
+                Bahwa status tanah tersebut adalah <strong><em>Tanah Milik Adat dan bukan Tanah Negara</em></strong>.</li>
+        </ol>
+        <p>
+            Demikian Surat Keterangan ini kami buat dengan sebenar-benarnya dan apabila isi Surat Keterangan ini tidak benar kami bertanggung jawab dan bersedia dituntut sesuai ketentuan hukum PIDANA maupun PERDATA.</p>
+        <p>
+            SAKSI-SAKSI YANG MEMBENARKAN KEPEMILIKANNYA:</p>
+        <table>
+            <tbody>
+                <tr>
+                    <td style="padding-right:20px;">
+                        1.</td>
+                    <td style="padding-right:100px;">
+                        Nama</td>
+                    <td width="300">
+                        : nama1</td>
+                    <td>
+                        (.....................................)</td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                    <td>
+                        Pekerjaan</td>
+                    <td>
+                        : pekerjaan1</td>
+                    <td>
+                        &nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                    <td>
+                        Alamat</td>
+                    <td>
+                        : alamat1</td>
+                    <td>
+                        &nbsp;</td>
+                </tr>
+            </tbody>
+        </table>
+        <br />
+        <table>
+            <tbody>
+                <tr>
+                    <td style="padding-right:20px;">
+                        2.</td>
+                    <td style="padding-right:100px;">
+                        Nama</td>
+                    <td width="300">
+                        : nama2</td>
+                    <td>
+                        (.....................................)</td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                    <td>
+                        Pekerjaan</td>
+                    <td>
+                        : pekerjaan2</td>
+                    <td>
+                        &nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                    <td>
+                        Alamat</td>
+                    <td>
+                        : alamat2</td>
+                    <td>
+                        &nbsp;</td>
+                </tr>
+            </tbody>
+        </table>
+        <p>
+            &nbsp;</p>
+        <table style="width: 100%;">
+            <tbody>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                    <td>
+                        &nbsp;</td>
+                    <td>
+                        tempat,tanggal</td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                    <td>
+                        &nbsp;</td>
+                    <td style="text-align: center;">
+                        KEPALA DESA/LURAH</td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                    <td>
+                        &nbsp;</td>
+                    <td style="text-align: center;">
+                        ....</td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                    <td>
+                        &nbsp;</td>
+                    <td style="text-align: center;">
+                        <br/><br/><br/><br/> </td>
+                </tr>
+                <tr>
+                    <td style="border: 1px #000 solid" width="300">
+                        <sub>* Tidak. diperbolehkan ada coretan, hapusan, ataupun beda huruf / tinta<br />
+                        </sub></td>
+                    <td>
+                        &nbsp;</td>
+                    <td style="text-align: center;" width="300">
+                        <u><strong>nama</strong></u></td>
+                </tr>
+            </tbody>
+        </table>
+        <p>
+            <br />
+            <br />
+            &nbsp;</p>
+    </body>
+</html>
---
+++ resources/views/pdf/suratkuasa.blade.php
@@ -0,0 +1,130 @@
+<html>
+    <head>
+        <title></title>
+        <style type="text/css">
+            table {
+                table-layout: auto;
+                border-collapse: collapse;
+            }
+            table td.absorbing-column {
+                width: 100%;
+            }
+            body {
+            font-family: 'times new roman';
+            }
+        </style>
+    </head>
+    <body>
+        <h3 style="text-align: center;">
+            <strong><u>SURAT KUASA</u></strong></h3>
+        <p>
+            Yang bertanda tangan di bawah ini:</p>
+        <table style="width: 100%;">
+            <tbody>
+                <tr>
+                    <td style="padding-right:20px;">
+                        I.</td>
+                    <td style="padding-right:100px;">
+                        Nama</td>
+                    <td class="absorbing-column">
+                        : nama1</td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                    <td>
+                        Pekerjaan</td>
+                    <td class="absorbing-column">
+                        : pekerjaan1</td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                    <td>
+                        Alamat</td>
+                    <td class="absorbing-column">
+                        : alamat1</td>
+                </tr>
+            </tbody>
+        </table>
+        <p>
+            Dalam hal ini bertindak untuk diri sendiri selaku pemilik tanah, yang selanjutnya disebut sebagai <strong>PIHAK PERTAMA</strong> (Pemberi Kuasa),</p>
+        <table style="width: 100%;">
+            <tbody>
+                <tr>
+                    <td style="padding-right:15px;">
+                        II.</td>
+                    <td style="padding-right:100px;">
+                        Nama</td>
+                    <td class="absorbing-column">
+                        : nama2</td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                    <td>
+                        Pekerjaan</td>
+                    <td class="absorbing-column">
+                        : pekerjaan2</td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                    <td>
+                        Alamat</td>
+                    <td class="absorbing-column">
+                        : alamat2</td>
+                </tr>
+            </tbody>
+        </table>
+        <p style="text-align: center;">
+            Dalam hal ini bertindak selaku <strong>PIHAK KEDUA</strong> ( Penerima Kuasa )&nbsp;</p>
+        <p style="text-align: center;">
+            Dengan ini Pihak Pertama memberi kuasa kepada Pihak Kedua.&nbsp;</p>
+        <style>
+            h4 { width:100%; text-align:center; border-bottom: 1px double #000; line-height:0.1em; margin:10px 0 20px; } 
+            h4 span { background:#fff; padding:0 10px; }
+        </style>
+        <br/>
+        <h4><span>KHUSUS</span></h4>
+        <p style="text-align: center;">
+            Untuk mengurus, menandatangani surat-surat yang diperlukan, menghadap kepada instansi yang berkepentingan, membayar biaya sesuai ketentuan, dan mengambil sertipikat atas nama pihak Pertama dan Kuasa ini tidak boleh dikuasakan lagi (di Subtitusikan) kepada pihak ketiga.</p>
+        <p style="text-align: center;">
+            Demikian Surat Kuasa ini dibuat dengan sebenarnya, dengan konsekwensi akibat hukum yang timbul sepenuhnya tanggung jawab penerima kuasa.</p>
+        <table style="width:100%;">
+            <tbody>
+                <tr>
+                    <td style="text-align:left; width:50%;">
+                        &nbsp;</td>
+                    <td style="width: 50%; text-align: right;">
+                        Kab. Bandung Barat, ###</td>
+                </tr>
+                <tr>
+                    <td style="text-align: center;">
+                        <strong>PIHAK KEDUA</strong></td>
+                    <td style="text-align: center;">
+                        <strong>PIHAK PERTAMA</strong></td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                    <td style="text-align: center;">
+                        <p>
+                            &nbsp;</p>
+                        <p>
+                            <sub><em>(Materai Rp. 6000,-)</em></sub></p>
+        <br/>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="text-align: center;">
+                        (###)</td>
+                    <td style="text-align: center;">
+                        (###)</td>
+                </tr>
+            </tbody>
+        </table>
+        <p>
+            &nbsp;</p>
+    </body>
+</html>
